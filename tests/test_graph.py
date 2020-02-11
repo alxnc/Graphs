b4c157346575be5380de4c8b4acdf07a589c3035
alxnc <alx_nc@protonmail.com>
2020-02-11 18:19:49 +0100

    update graphs.py and test_graph.py

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -11,17 +11,23 @@ import unittest
 
 class TestGraphs(unittest.TestCase):
 
-    def test_chkBracket1_(self):
+    def test_chkBracket1(self):
         self.assertEqual(graphs.checkBrackets("123456"), 0, "Should be 0")
 
-    def test_chkBracket2_(self):
+    def test_chkBracket2(self):
         self.assertEqual(graphs.checkBrackets("12+(3*4)+56"), 1, "Should be 1")
 
-    def test_chkBracket3_(self):
+    def test_chkBracket3(self):
         self.assertEqual(graphs.checkBrackets("12+(3*4+56"), -1, "Should be -1")
+
+    def test_analizeOprs_ok(self):
+        self.assertEqual(graphs.analizeOperations("1+2+3+4"), True, "Should be true")
+
+    def test_analizeOprs_nok(self):
+        self.assertEqual(graphs.analizeOperations("++1+2+3+4"), False, "Should be false")
+
+    def test_analizeOprs_nok2(self):
+        self.assertEqual(graphs.analizeOperations("1+2++3+4"), False, "Should be false")
 
 if __name__ == '__main__':
     unittest.main()
-
-
-    
