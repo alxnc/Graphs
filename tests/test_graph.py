501e3c5d0cc3accc045e1356071ec6fd0c1a3aa4
alxnc <alx_nc@protonmail.com>
2020-02-20 10:29:21 +0100

    Update tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -12,13 +12,13 @@ import unittest
 class TestGraphs(unittest.TestCase):
 
     def test_chkBracket1(self):
-        self.assertEqual(graphs.checkBrackets("123456"), 0, "Should be 0")
+        self.assertEqual(graphs.checkBrackets("123456"), 1, "Should be 1")
 
     def test_chkBracket2(self):
         self.assertEqual(graphs.checkBrackets("12+(3*4)+56"), 1, "Should be 1")
 
     def test_chkBracket3(self):
-        self.assertEqual(graphs.checkBrackets("12+(3*4+56"), -1, "Should be -1")
+        self.assertEqual(graphs.checkBrackets("12+(3*4+56"), 0, "Should be 0")
 
     def test_analizeOprs_ok(self):
         self.assertEqual(graphs.analizeOperations("1+2+3+4"), True, "Should be true")
@@ -28,6 +28,12 @@ class TestGraphs(unittest.TestCase):
 
     def test_analizeOprs_nok2(self):
         self.assertEqual(graphs.analizeOperations("1+2++3+4"), False, "Should be false")
+
+    def test_evalexpr_ok(self):
+        self.assertEqual(graphs.evalExpr("12+2*(3*4+10/5)"), 40, "Should be 40")
+        
+    def test_evalexpr_ok1(self):
+        self.assertEqual(graphs.evalExpr("3^3^2"), 19683, "Should be 19683")
 
 if __name__ == '__main__':
     unittest.main()
